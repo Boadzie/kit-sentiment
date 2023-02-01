a9e80ce8bd7f9a608ef504c7ca75738c2fe5a90e
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-01 08:26:43 +0000

    Add navbar to app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.postcss';
 </script>
-
+<div class="mb-8 navbar bg-slate-800 rounded-sm">
+  <a class="text-white btn btn-ghost normal-case text-xl">Kit-Sentiment</a>
+</div>
+<main>
 <slot />
+</main>
