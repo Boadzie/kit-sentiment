308bff2139b88e85173f51f2399911ad1253bf14
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-01 11:06:52 +0000

    Modify styles on layout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.postcss';
 </script>
 <div class="mb-8 navbar bg-slate-800 rounded-sm">
-  <a class="text-white btn btn-ghost normal-case text-xl">Kit-Sentiment</a>
+  <a href="/" class="text-white btn btn-ghost normal-case text-xl">Kit-Sentiment</a>
 </div>
 <main>
 <slot />
