3db6db0aa21e0934f89bb5e8eeb58f023710a1f9
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-01 11:24:35 +0000

    Add progressive enhancement to form
    - Modify text on page
    - Make component responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,20 @@
 <script>
+import {enhance} from "$app/forms"
 export let form;
 
 </script>
-<section class="container mx-auto px-4 py-5">
-<div class="flex justify-center items-center">
- <div class="flex gap-4 items-center justify-between">
+<section class="container mx-auto px-4 pb-5">
+<div class="flex lg:flex-row justify-center items-center">
+ <div class="flex-col flex lg:flex-row gap-4 items-center justify-between">
    <div class="w-full lg:w-1/2">
     <img class="rounded-sm brightness-50" src="group-chat.svg" alt=""/>
    </div>
-   <div class="w-full lg:w-1/2 bg-slate-100 p-8 h-full">
+   <div class="w-full lg:w-1/2 bg-slate-100 p-3 lg:p-8 h-full">
     <h2 class="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-slate-500 to-pink-600 my-4">Kit-Sentiment</h2>
-    <p class="text-lg text-slate-500">Kit-sentiment is a powerful and intuitive Svelte app that leverages Artificial Intelligence to analyze the sentiment behind any text input, including social media posts. With its user-friendly interface, you can easily input your text and receive an accurate sentiment analysis in just a matter of seconds. Whether you're curious about the tone behind a friend's tweet or looking to gain insights into public opinion, Kit-sentiment has got you covered. So why wait? Give it a try now and experience the power of AI-powered sentiment analysis!</p>
-     <form method="POST" class="w-full my-6" action="">
+    <p class="text-lg text-slate-500">Kit-sentiment is a simple and intuitive Svelte app that leverages Artificial Intelligence to analyze the sentiment behind any text input, including social media posts. With its user-friendly interface, you can easily input your text and receive an accurate sentiment analysis in just a matter of seconds. Whether you're curious about the tone behind a friend's tweet or looking to gain insights into public opinion, Kit-sentiment has got you covered. So why wait? Give it a try now and experience the power of AI-powered sentiment analysis!</p>
+     <form method="POST" class="w-full my-6" action="" use:enhance>
      <div>
-      <textarea rows="4" cols="7" name="text"  class="w-full h-98 rounded-sm textarea" placeholder="Your text...."></textarea>
+      <textarea rows="4" cols="7" name="text"  class="w-full h-98 rounded-sm textarea text-slate-500 text-md" placeholder="Your text...."></textarea>
      </div>
      {#if form?.errors?.text }
       <span class="py-2 px-1 text-red-400">{form?.errors?.text[0]}</span>
