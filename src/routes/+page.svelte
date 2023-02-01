a4e8b69b358735220a074e0c05f66fb8d67e0986
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-01 08:26:22 +0000

    Add homapage with form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="container mx-auto px-4">
+<div class="flex justify-center items-center">
+ <div class="flex gap-4 items-center justify-between">
+   <div class="w-full lg:w-1/2">
+    <img class="rounded-sm brightness-50" src="group-chat.svg" alt=""/>
+   </div>
+   <div class="w-full lg:w-1/2 bg-slate-100 p-8 h-full">
+    <h2 class="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-slate-500 to-pink-600 my-4">Kit-Sentiment</h2>
+    <p class="text-lg text-slate-500">Kit-sentiment is a powerful and intuitive Svelte app that leverages Artificial Intelligence to analyze the sentiment behind any text input, including social media posts. With its user-friendly interface, you can easily input your text and receive an accurate sentiment analysis in just a matter of seconds. Whether you're curious about the tone behind a friend's tweet or looking to gain insights into public opinion, Kit-sentiment has got you covered. So why wait? Give it a try now and experience the power of AI-powered sentiment analysis!</p>
+     <form class="w-full my-6" action="">
+     <div>
+      <textarea rows="4" cols="7"  class="w-full h-98 rounded-sm textarea" placeholder="Your text...."></textarea>
+     </div>
+     <div class="my-4">
+      <button class="rounded-sm btn bg-slate-700 hover:bg-slate-600 text-white btn-outline w-1/3" type="submit">Submit</button>
+     </div>
+     </form>
+   </div>
+ </div>
+</div>
+</section>
