f14b4087686aef5ef66f170048a9bc33d89fe78c
Daniel Boadzie <boadziedaniel43@gmail.com>
2023-02-01 11:08:01 +0000

    Get form values in the component
    - Display form errors if any
    - Display form result when you get it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<section class="container mx-auto px-4">
+<script>
+export let form;
+
+</script>
+<section class="container mx-auto px-4 py-5">
 <div class="flex justify-center items-center">
  <div class="flex gap-4 items-center justify-between">
    <div class="w-full lg:w-1/2">
@@ -7,14 +11,22 @@
    <div class="w-full lg:w-1/2 bg-slate-100 p-8 h-full">
     <h2 class="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-slate-500 to-pink-600 my-4">Kit-Sentiment</h2>
     <p class="text-lg text-slate-500">Kit-sentiment is a powerful and intuitive Svelte app that leverages Artificial Intelligence to analyze the sentiment behind any text input, including social media posts. With its user-friendly interface, you can easily input your text and receive an accurate sentiment analysis in just a matter of seconds. Whether you're curious about the tone behind a friend's tweet or looking to gain insights into public opinion, Kit-sentiment has got you covered. So why wait? Give it a try now and experience the power of AI-powered sentiment analysis!</p>
-     <form class="w-full my-6" action="">
+     <form method="POST" class="w-full my-6" action="">
      <div>
-      <textarea rows="4" cols="7"  class="w-full h-98 rounded-sm textarea" placeholder="Your text...."></textarea>
+      <textarea rows="4" cols="7" name="text"  class="w-full h-98 rounded-sm textarea" placeholder="Your text...."></textarea>
      </div>
+     {#if form?.errors?.text }
+      <span class="py-2 px-1 text-red-400">{form?.errors?.text[0]}</span>
+    {/if}
      <div class="my-4">
       <button class="rounded-sm btn bg-slate-700 hover:bg-slate-600 text-white btn-outline w-1/3" type="submit">Submit</button>
      </div>
      </form>
+     <div>
+       {#if form?.success }
+        <p class="font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-slate-500 to-pink-600">{form?.sentiment}</p>
+       {/if}
+     </div>
    </div>
  </div>
 </div>
